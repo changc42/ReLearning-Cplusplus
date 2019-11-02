3fa1e5b3068d9347c82159bca80aaa1b7949149c
Caleb Chang <changc42@gmail.com>
2019-11-02 13:44:25 -0400

    created bin folder

--- stringOutput.cpp
+++ stringOutput.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include<cmath>
 
 using namespace std;
 
 int main()
 {
+	int x =2;
+	int y=3;
+	cout << pow(x,y) << endl;
     string yourMom = "yourMOM";
     cout << yourMom << endl;
     return 0;
