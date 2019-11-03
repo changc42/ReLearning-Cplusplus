22d8125b1e6f42ced85f93e67480f45a4bdf321a
Caleb Chang <changc42@gmail.com>
2019-11-03 00:07:03 -0400

    added 2-dim array in form of double pointer

--- n_queens.cpp
+++ n_queens.cpp
@@ -1,12 +1,38 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 class Board{
+	public:
 	int rows;
 	int cols;
-	int b[rows][cols];
+	int **b;
+	
+	Board(int r, int c){
+		rows=r;
+		cols=c;
+		b=new int*[r];
+		for(int i=0; i<r; i++){
+			b[i] = new int[c];
+		}
+	}
+	
+	void print(){
+		for(int i=0; i<8; i++){
+			for(int j=0; j<8; j++){
+				cout << b[i][j];
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+	
 };
+
 
 int main(){
 	
+	Board board(8,8);
+	fill(board.b[0], board.b[0]+64, 3);
+	board.print();
 }
